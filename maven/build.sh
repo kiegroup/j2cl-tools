1b7c67ba46bc2c449fbf4dfb69d8c92e4a75ff09
Colin Alworth <colin@colinalworth.com>
2022-03-04 16:53:37 -0600

    Incorporate javadoc into the bazel and maven builds
    Also updates the root pom to include specifics that sonatype will
    require for deployment.

--- maven/build.sh
+++ maven/build.sh
@@ -3,7 +3,7 @@
 set -e
 BAZEL=${BAZEL:-bazel}
 
-${BAZEL} build //transpiler/java/com/google/j2cl/transpiler:*
+${BAZEL} build //transpiler/java/...:*
 
 ${BAZEL} build //tools/java/com/google/j2cl/tools/gwtincompatible:*
 ${BAZEL} build //tools/java/com/google/j2cl/tools/minifier:*
@@ -14,7 +14,7 @@ ${BAZEL} build //jre/java:*
 
 ${BAZEL} build //junit/emul/java:*
 ${BAZEL} build //jre/javatests/com/google/gwt/junit:*
-${BAZEL} build  //junit/generator/java/com/google/j2cl/junit/async:*
+${BAZEL} build //junit/generator/java/com/google/j2cl/junit/async:*
 ${BAZEL} build //junit/generator/java/com/google/j2cl/junit/apt:*
 
 ${BAZEL} build @org_gwtproject_gwt//user:libgwt-javaemul-internal-annotations.jar
